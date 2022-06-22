1ad41444c2e7bd0a382a03279623db7c9f20ea26
Julio-segfault <79867439+Julio-segfault@users.noreply.github.com>
2022-06-22 16:45:35 -0300

    Add files via upload

---
+++ Semana07-2021-2/problema04/Test.py
@@ -0,0 +1,14 @@
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+#from kivy.uix.screenmanager import ScreenManager, Screen
+
+#class Gerenciador(Screen):
+    #pass
+
+class Test(App):
+    pass
+
+Test().run()
---
+++ Semana07-2021-2/problema04/main.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
+#from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class Whats(App):
+    pass
+
+if __name__ == "__main__":
+    Whats().run()
